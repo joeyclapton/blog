84beb93cafa542af4490e4a7e3f49f2dbd338ca9
joeyclapton <joeyclapton42@gmail.com>
2023-08-10 13:10:03 -0300

    fist commit

---
+++ site.config.js
@@ -0,0 +1,71 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "Joey Clapton",
+    image: "/avatar.webp", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "Front-end developer",
+    bio: "",
+    email: "joeyclapton42@gmail.com",
+    linkedin: "joeyclapton",
+    github: "joeyclapton",
+    instagram: "",
+  },
+  projects: [
+    // {
+    //   name: `morethan-log`,
+    //   href: "https://github.com/joeyclapton/blog",
+    // },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "Joey Clapton",
+    description: "welcome to my personal blog!",
+    theme: "auto", // ['light', 'dark', 'auto']
+  },
+
+  // CONFIG configration (required)
+  link: "https://joeyclapton.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  seo: {
+    keywords: ["Blog", "Website", "Notion"],
+  },
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: true,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: true,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: "joeyclapton/blog",
+      "issue-term": "og:title",
+      label: "💬 Utterances"
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: process.env.CUSDIS_APP_ID || "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+}
+
+module.exports = { CONFIG }
